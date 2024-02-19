c66899796283b9f92c937c2ad6d2f6e52dfc38c7
ShaunAnderson17 <118745439+ShaunAnderson17@users.noreply.github.com>
2024-02-18 19:14:55 -0500

    search bar completed

--- src/ui-components/searchbar.tsx
+++ src/ui-components/searchbar.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
- 
-function SearchBar() { 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';    
+
+function SearchBar() {
+    const [searchTerm, setSearchTerm] = useState('');
+    const navigate = useNavigate();   
+
+    const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setSearchTerm(event.target.value);
+    };
+
+    const handleSearchSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        navigate(`/SearchResults/${searchTerm}`);   
+    };
+
     return (
         <div className="search-wrapper">
-            <label htmlFor="search"> Search Movies</label>
-            <input type="search" id="search" />
+            <form onSubmit={handleSearchSubmit}>
+                <label htmlFor="search">Search Movies</label>
+                <input type="search" id="search" value={searchTerm} onChange={handleSearchChange} />
+                <button type="submit">Search</button>
+            </form>
         </div>
     );
 }

b094d51e575615e8513d6ad1b8fdb5d7eb5b8313
ShaunAnderson17 <118745439+ShaunAnderson17@users.noreply.github.com>
2024-02-20 01:15:45 -0500

    added api to new releases

--- src/pages/newreleases.tsx
+++ src/pages/newreleases.tsx
@@ -1,3 +1,74 @@
-export default function NewReleases() {
-    return <h1>New Releases</h1>
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TMDB_API, OMDB_API } from '../api';
+ 
+interface MovieData {
+    id: number;
+    title: string;
+    poster_path: string;
+    release_date: string;
+    mpaaRating: string;
+    runtime: string;
 }
+
+function NewReleases() {
+    const [movies, setMovies] = useState<MovieData[]>([]);
+
+    const fetchLatestMovies = async () => {
+        try {
+            const response = await TMDB_API.get('/movie/now_playing', {
+                params: {
+                    page: 1,
+                },
+            });
+
+            const moviesWithRatings = await Promise.all(response.data.results.map(async (movie: MovieData) => {
+                const omdbResponse = await OMDB_API.get('/', {
+                    params: {
+                        t: movie.title,
+                        plot: 'short',
+                        r: 'json'
+                    }
+                });
+                return {
+                    ...movie,
+                    release_date: movie.release_date,
+                    mpaaRating: omdbResponse.data.Rated,
+                    runtime: omdbResponse.data.Runtime
+                };
+            }));
+
+            setMovies(moviesWithRatings);
+        } catch (error) {
+            console.error('Error fetching latest movies:', error);
+        }
+    };
+
+    useEffect(() => {
+        fetchLatestMovies();
+    }, []);
+
+    return (
+        <div className="latest-movies">
+            <h2>New Releases</h2>
+            <ul>
+                {movies.map((movie) => (
+                    <li key={movie.id}>
+                        <h3>{movie.title}</h3>
+                        <p>Release Date: {movie.release_date}</p>
+                        <p>MPAA Rating: {movie.mpaaRating}</p>
+                        <p>Runtime: {movie.runtime}</p>
+                        <Link to={`/movie/${movie.id}`}>
+                            <img
+                                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                                alt={movie.title}
+                            />
+                        </Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default NewReleases;

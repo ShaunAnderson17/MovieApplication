f9eb23b7927542fb9bf3da325e6f821e25cf3da2
ShaunAnderson17 <118745439+ShaunAnderson17@users.noreply.github.com>
2024-03-01 22:23:26 -0500

    made new releases the start page

--- src/ui-components/navbar.tsx
+++ src/ui-components/navbar.tsx
@@ -1,36 +1,52 @@
-import { useImperativeHandle } from "react";
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath, useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
-     
+    const navigate = useNavigate();
+    const [initialRedirect, setInitialRedirect] = useState(true);
+
+    useEffect(() => { 
+        if (initialRedirect) {
+            navigate("/NewReleases");
+            setInitialRedirect(false);
+        }
+    }, [navigate, initialRedirect]);
+
     return (
         <nav className="nav">
-            <Link to="/" className="MovieTitle">Movie App</Link>
             <ul>
-                <CustomLink to="/NewReleases" className="NewReleases">New Releases</CustomLink>
-                <CustomLink to="/Browse" className="Browse">Browse</CustomLink>
-                <CustomLink to="/AdvancedSearch" className="AdvancedSearch">Advanced Search</CustomLink>
-
+                <CustomLink to="/NewReleases" className="NewReleases">
+                    New Releases
+                </CustomLink>
+                <CustomLink to="/PopularMovies" className="PopularMovies">
+                    Popular Movies
+                </CustomLink>
+                <CustomLink to="/Browse" className="Browse">
+                    Browse
+                </CustomLink>
+                <CustomLink to="/AdvancedSearch" className="AdvancedSearch">
+                    Advanced Search
+                </CustomLink>
             </ul>
         </nav>
-    )
+    );
 }
- 
+
 interface CustomLinkProps {
     to: string;
     children: React.ReactNode;
     className?: string;
 }
- 
-function CustomLink({ to, children, ...props }: CustomLinkProps) { 
-    const resolvedPath = useResolvedPath(to) 
-    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
-     
+
+function CustomLink({ to, children, ...props }: CustomLinkProps) {
+    const resolvedPath = useResolvedPath(to);
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true });
+
     return (
-        <li className={isActive ? "active" : ""}>
+        <li className={isActive ? "active" : ""} style={{ listStyleType: "none" }}>
             <Link to={to} className={props.className} {...props}>
                 {children}
             </Link>
         </li>
     );
-} 
+}
